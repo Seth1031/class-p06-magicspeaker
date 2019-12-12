204e04166db20e11922533e91e9edef700cbc2d3
Seth <skeror@gmail.com>
2019-12-12 15:36:59 +0800

    Initial Commit

---
+++ class-p06/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  class-p06
+//
+//  Created by Seth on 2019/12/12.
+//  Copyright © 2019 Seth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
